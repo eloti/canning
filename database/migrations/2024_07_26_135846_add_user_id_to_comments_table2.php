8cfcab0d858658654e22c2e2369f38c646b539c2
matsrgb <mats.rgb@outlook.com>
2024-07-26 10:59:50 -0300

    Update 2024_07_26_135846_add_user_id_to_comments_table2.php

--- database/migrations/2024_07_26_135846_add_user_id_to_comments_table2.php
+++ database/migrations/2024_07_26_135846_add_user_id_to_comments_table2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserIdToCommentsTable extends Migration
+class AddUserIdToCommentsTable2 extends Migration
 {
     /**
      * Run the migrations.
